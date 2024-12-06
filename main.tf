7161ce337faa303a62b19b948ae7ce095a1e93a9
Yolimar Rios <yolimar.rios23@gmail.com>
2024-12-05 23:32:00 -0500

    fixing resources references

--- main.tf
+++ main.tf
@@ -20,13 +20,13 @@ variable "postgres_password" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "my-custom-mode-network"
+  name                    = "my-custom1-mode-network"
   auto_create_subnetworks = false
   mtu                     = 1460
 }
 # Create a subnet
 resource "google_compute_subnetwork" "default" {
-  name          = "my-subnet"
+  name          = "my-subnet1"
   ip_cidr_range = "10.0.1.0/24"
   region        = "us-central1"
   network       = google_compute_network.vpc_network.id
